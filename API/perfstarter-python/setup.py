7dc87f5c9f39700f509714251e342ce3aafbcdaf
Keshav Deshpande <50804456+deshkeshpande@users.noreply.github.com>
2020-10-14 11:22:39 -0400

    Add files via upload

---
+++ API/perfstarter-python/setup.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+
+import sys
+from setuptools import setup, find_packages
+
+NAME = "swagger_server"
+VERSION = "1.0.0"
+# To install the library, run the following
+#
+# python setup.py install
+#
+# prerequisite: setuptools
+# http://pypi.python.org/pypi/setuptools
+
+REQUIRES = ["connexion"]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="KAIROS Performer implementation API",
+    author_email="",
+    url="",
+    keywords=["Swagger", "KAIROS Performer implementation API"],
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    package_data={'': ['swagger/swagger.yaml']},
+    include_package_data=True,
+    entry_points={
+        'console_scripts': ['swagger_server=swagger_server.__main__:main']},
+    long_description="""\
+    This is the specification for the KAIROS Performer Implementation API, that every performer container implementation will need to implement.
+    """
+)
